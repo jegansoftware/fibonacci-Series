b496cb5156e7875036a742a9d3811a175dbce8ae
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-06-22 10:43:40 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package analyze;
+
+public class finocci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FinocciSerious je=new FinocciSerious();
+		je.read();
+	}
+
+}
